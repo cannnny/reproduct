0c7c8dec4f59ae2250365eb77d87393ba2777f31
cany <couleurvide1008@gmail.com>
2023-06-18 21:23:53 +0900

    コードレビューを踏まえて修正

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className='App'>
       <header>
-        <div className='container'>
-          <div className='headerLogo'>
+        <div className='container header_inner'>
+          <div className='header_logo'>
             <img src='/img/logo.svg'  alt='' />
           </div>
-          <div className='headerMenu'>
+          <div className='header_menu'>
             <ul>
               <li><a href='#about'>About</a></li>
               <li><a href='#bicycle'>Bicycle</a></li>
@@ -17,41 +17,37 @@ function App() {
           </div>
         </div>
       </header>
+      <img className='cover_img' src='/img/mainvisual.jpg' alt='' />
       <div className='main'>
-        <div className='container'>
-          <img src='/img/mainvisual.jpg' alt='' />
+        <div className='container main_inner'>
           <div id='about'>
-            <div className='contents'>
-              <h1><span className='heading'>About</span></h1>
-              <img className='icon' src='/img/about.jpg' alt='' />
-              <h2>KAKERU MIYAICHI</h2>
-              <p>テキスト</p>
-            </div>
+            <h2><span className='heading'>About</span></h2>
+            <img className='icon' src='/img/about.jpg' alt='' />
+            <h3>KAKERU MIYAICHI</h3>
+            <p>テキスト</p>
           </div>
           <div id='bicycle'>
-            <div className='contents'>
-              <h1><span className='heading'>Bicycle</span></h1>
-              <div className='photos'>
-                <img className='photo' src='/img/bicycle1.jpg' alt='' />
-                <h2>タイトル</h2>
-                <p>テキスト</p>
-              </div>
-              <div className='photos'>
-                <img className='photo' src='/img/bicycle2.jpg' alt='' />
-                <h2>タイトル</h2>
-                <p>テキスト</p>
-              </div>
-              <div className='photos'>
-                <img className='photo' src='/img/bicycle3.jpg' alt='' />
-                <h2>タイトル</h2>
-                <p>テキスト</p>
-              </div>
+            <h2><span className='heading'>Bicycle</span></h2>
+            <div className='photos'>
+              <img className='photo' src='/img/bicycle1.jpg' alt='' />
+              <h3>タイトル</h3>
+              <p>テキスト</p>
+            </div>
+            <div className='photos'>
+              <img className='photo' src='/img/bicycle2.jpg' alt='' />
+              <h3>タイトル</h3>
+              <p>テキスト</p>
+            </div>
+            <div className='photos'>
+              <img className='photo' src='/img/bicycle3.jpg' alt='' />
+              <h3>タイトル</h3>
+              <p>テキスト</p>
             </div>
           </div>
         </div>
       </div>
       <footer>
-        <div className='container'>
+        <div className='container footer_inner'>
           <p>@ 2020 Profile</p>
         </div>
       </footer>
